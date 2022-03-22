5a3f66c30c7f1b170fa480d36a5b88a02557e2c3
Jettik1 <contrakt120@gmail.com>
2022-03-22 12:16:07 +0300

    add deviceController logic and fix ErrorHandlingMiddleware

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,31 +1,69 @@
-const {v4: uuidv4} = require('uuid')
-const path = require('path')
-const {Device} = require('../models/models');
-const { nextTick } = require('process');
-const ApiError = require('../error/Apierror');
+const uuid = require('uuid')
+const path = require('path');
+const {Device, DeviceInfo} = require('../models/models')
+const ApiError = require('../error/ApiError');
 
 class DeviceController {
     async create(req, res, next) {
         try {
-            const {name, price, brandId, typeId, info} = req.body;
+            let {name, price, brandId, typeId, info} = req.body
             const {img} = req.files;
-            let fileName = uuidv4() + ".jpg";
-            img.mv(__dirname, '..','static', fileName)
+            let hexString = uuid.v4(); // генерируем строку
+            hexString = hexString.replace(/-/g, ""); // убираем лишние(декоративные) знаки
+            let base64String = Buffer.from(hexString, 'hex').toString('base64'); // получаем рандомную строку из рандомной hex-строки
+            let fileName = `${brandId}_${name}_${uuid.v4()}.jpg`; // Создаем имя файла
+            img.mv(path.resolve(__dirname, '..', 'static', fileName));
+            const device = await Device.create({name, price, brandId, typeId, img: fileName});
 
-            const device = await Device.create({name, price, brandId, typeId, img: fileName})
+            if (info) {
+                info = JSON.parse(info)
+                info.forEach(i =>
+                    DeviceInfo.create({ // не понял почему не ставим await
+                        title: i.title,
+                        description: i.description,
+                        deviceId: device.id
+                    })
+                )
+            }
+
             return res.json(device)
-        } catch (error) {
-            next(ApiError.badRequest(error.message))
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
         }
+
     }
 
     async getAll(req, res) {
-        
+        let {brandId, typeId, limit, page} = req.query
+        page = page || 1
+        limit = limit || 10
+        let offset = page * limit - limit
+        let devices;
+        if (!brandId && !typeId) {
+            devices = await Device.findAndCountAll({limit, offset})
+        }
+        if (brandId && !typeId) {
+            devices = await Device.findAndCountAll({where:{brandId}, limit, offset})
+        }
+        if (!brandId && typeId) {
+            devices = await Device.findAndCountAll({where:{typeId}, limit, offset})
+        }
+        if (brandId && typeId) {
+            devices = await Device.findAndCountAll({where:{typeId, brandId}, limit, offset})
+        }
+        return res.json(devices)
     }
 
     async getOne(req, res) {
-        
+        const {id} = req.params;
+        const device = await Device.findOne(
+            {
+                where: {id},
+                include: [{model: DeviceInfo, as: 'info'}]
+            },
+        )
+        return res.json(device)
     }
 }
 
-module.exports = new DeviceController();
+module.exports = new DeviceController()
