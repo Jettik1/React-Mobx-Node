00fed13d3638aab43b675a6c6f73f1e165b00c40
Jettik1 <contrakt120@gmail.com>
2022-05-02 11:00:59 +0300

    add device and characteristics

--- client/src/components/modals/CreateBrand.js
+++ client/src/components/modals/CreateBrand.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { Button, Form, Modal } from "react-bootstrap";
+import { useState } from "react";
+import { createBrand } from "../../http/deviceAPI";
 
 const CreateBrand = ({ show, onHide }) => {
+  const [value, setValue] = useState("");
+
+  const addBrand = () => {
+    createBrand({ name: value }).then((data) => {
+      setValue("");
+      onHide();
+    });
+  };
+
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
       <Modal.Header closeButton>
@@ -11,14 +22,18 @@ const CreateBrand = ({ show, onHide }) => {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control placeholder="Введите название бренда" />
+          <Form.Control
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Введите название бренда"
+          />
         </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="outline-danger" onClick={onHide}>
           Закрыть
         </Button>
-        <Button variant="outline-success" onClick={onHide}>
+        <Button variant="outline-success" onClick={addBrand}>
           Добавить
         </Button>
       </Modal.Footer>
--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,15 +1,41 @@
-import React, { useContext, useState } from "react";
+import { observer } from "mobx-react-lite";
+import React, { useContext, useEffect, useState } from "react";
 import { Button, Col, Dropdown, Form, Modal, Row } from "react-bootstrap";
 import { Context } from "../..";
+import { fetchBrands, fetchTypes } from "../../http/deviceAPI";
 
-const CreateDevice = ({ show, onHide }) => {
+const CreateDevice = observer(({ show, onHide }) => {
   const { device } = useContext(Context);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [file, setFile] = useState(null);
   const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    fetchTypes().then((data) => device.setTypes(data)); // Берём Типы из базы данных
+    fetchBrands().then((data) => device.setBrands(data)); // Берём Бренды из БД
+  }, []);
+
   const addInfo = () => {
     setInfo([...info, { title: "", description: "", number: Date.now() }]);
   };
   const removeInfo = (number) => {
     setInfo(info.filter((i) => i.number !== number));
+  };
+
+  const changeInfo = (key, value, number) => {
+    // ??
+    setInfo(
+      info.map((i) => (i.number === number ? { ...i, [key]: value } : i))
+    );
+  };
+
+  const selectFile = (e) => {
+    setFile(e.target.file[0]);
+  };
+
+  const addDevice = () => {
+    console.log(info);
   };
 
   return (
@@ -22,31 +48,49 @@ const CreateDevice = ({ show, onHide }) => {
       <Modal.Body>
         <Form>
           <Dropdown className="mt-2 mb-2">
-            <Dropdown.Toggle>Выберите тип</Dropdown.Toggle>
+            <Dropdown.Toggle>
+              {device.selectedType || "Выберите тип"}
+            </Dropdown.Toggle>
             <Dropdown.Menu>
               {device.types.map((type) => (
-                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+                <Dropdown.Item
+                  onClick={() => device.setSelectedType(type)}
+                  key={type.id}
+                >
+                  {type.name}
+                </Dropdown.Item>
               ))}
             </Dropdown.Menu>
           </Dropdown>
           <Dropdown className="mt-2 mb-2">
-            <Dropdown.Toggle>Выберите автора</Dropdown.Toggle>
+            <Dropdown.Toggle>
+              {device.selectedBrand || "Выберите бренд"}
+            </Dropdown.Toggle>
             <Dropdown.Menu>
               {device.brands.map((brand) => (
-                <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+                <Dropdown.Item
+                  onClick={() => device.setSelectedBrand(brand)}
+                  key={brand.id}
+                >
+                  {brand.name}
+                </Dropdown.Item>
               ))}
             </Dropdown.Menu>
           </Dropdown>
           <Form.Control
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className="mt-3"
             placeholder="Введите название товара"
           />
           <Form.Control
+            value={price}
+            onChange={(e) => setPrice(Number(e.target.value))}
             className="mt-3"
             placeholder="Введите стоимость товара"
             type="number"
           />
-          <Form.Control className="mt-3" type="file" />
+          <Form.Control className="mt-3" type="file" onChange={selectFile} />
           <hr />
           <Button variant="outline-dark" onClick={addInfo}>
             Добавить описание
@@ -54,10 +98,22 @@ const CreateDevice = ({ show, onHide }) => {
           {info.map((i) => (
             <Row className="mt-2" key={i.number}>
               <Col md={4}>
-                <Form.Control placeholder="Введите название характеристики" />
+                <Form.Control
+                  value={i.title}
+                  onChange={(e) =>
+                    changeInfo("title", e.target.value, i.number)
+                  }
+                  placeholder="Введите название характеристики"
+                />
               </Col>
               <Col md={4}>
-                <Form.Control placeholder="Введите описание характеристики" />
+                <Form.Control
+                  value={i.description}
+                  onChange={(e) =>
+                    changeInfo("description", e.target.value, i.number)
+                  }
+                  placeholder="Введите описание характеристики"
+                />
               </Col>
               <Col md={4}>
                 <Button
@@ -75,12 +131,12 @@ const CreateDevice = ({ show, onHide }) => {
         <Button variant="outline-danger" onClick={onHide}>
           Закрыть
         </Button>
-        <Button variant="outline-success" onClick={onHide}>
+        <Button variant="outline-success" onClick={addDevice}>
           Добавить
         </Button>
       </Modal.Footer>
     </Modal>
   );
-};
+});
 
 export default CreateDevice;
